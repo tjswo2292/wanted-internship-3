83e7a721cf7ddf80f6c259469dee39c2f3ea6283
seonjae <tjswo2292@naver.com>
2023-09-18 15:43:12 +0900

    feat: useCharData custom hook으로 버튼 관리

--- client/src/components/main/FilterButton.tsx
+++ client/src/components/main/FilterButton.tsx
@@ -1,31 +1,16 @@
 import styled from 'styled-components'
-import { ChartData, Chart as ChartJS, ChartTypeRegistry } from 'chart.js'
-import { removeDuplicatesArray } from '../../util/removeDuplicatesArray'
-import { useContext } from 'react'
-import { ColorContext } from '../../context/colorProvider'
-import { updateDatasets } from '../../util/updateDatasets'
 
-interface FilterButtonProps {
-  regionArray: string[]
-  chartRef: React.RefObject<ChartJS<keyof ChartTypeRegistry>>
-  chartProps: ChartData<'line' | 'bar'>
-}
+import useChartData from '../../hook/useChartData'
 
-const FilterButton = ({ regionArray, chartProps }: FilterButtonProps) => {
-  const { updateColorArray } = useContext(ColorContext)
-
-  const regionItem = removeDuplicatesArray(regionArray)
-
-  const handleSortingButtonClick = (buttonTitle: string) => {
-    updateColorArray(updateDatasets(buttonTitle, chartProps))
-  }
+const FilterButton = () => {
+  const { regionItem, handleRegionButton } = useChartData()
 
   return (
     <Box>
       <Wrapper>
         {regionItem.map(element => (
           <ButtonItem key={element}>
-            <Button onClick={() => handleSortingButtonClick(element)}>{element}</Button>
+            <Button onClick={() => handleRegionButton(element)}>{element}</Button>
           </ButtonItem>
         ))}
       </Wrapper>
