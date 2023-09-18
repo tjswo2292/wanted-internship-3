ce0cd41628d13406e28c6fe8f3d35bec827b70b5
seonjae <tjswo2292@naver.com>
2023-09-18 11:20:16 +0900

    feat: 버튼 클릭시 해당하는 지역 하이라이트

--- client/src/components/main/FilterButton.tsx
+++ client/src/components/main/FilterButton.tsx
@@ -1,19 +1,31 @@
 import styled from 'styled-components'
+import { ChartData, Chart as ChartJS, ChartTypeRegistry } from 'chart.js'
 import { removeDuplicatesArray } from '../../util/removeDuplicatesArray'
+import { useContext } from 'react'
+import { ColorContext } from '../../context/colorProvider'
+import { updateDatasets } from '../../util/updateDatasets'
 
 interface FilterButtonProps {
   regionArray: string[]
+  chartRef: React.RefObject<ChartJS<keyof ChartTypeRegistry>>
+  chartProps: ChartData<'line' | 'bar'>
 }
 
-const FilterButton = ({ regionArray }: FilterButtonProps) => {
+const FilterButton = ({ regionArray, chartProps }: FilterButtonProps) => {
+  const { updateColorArray } = useContext(ColorContext)
+
   const regionItem = removeDuplicatesArray(regionArray)
+
+  const handleSortingButtonClick = (buttonTitle: string) => {
+    updateColorArray(updateDatasets(buttonTitle, chartProps))
+  }
 
   return (
     <Box>
       <Wrapper>
         {regionItem.map(element => (
           <ButtonItem key={element}>
-            <Button>{element}</Button>
+            <Button onClick={() => handleSortingButtonClick(element)}>{element}</Button>
           </ButtonItem>
         ))}
       </Wrapper>
