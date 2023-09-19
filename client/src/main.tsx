54b0561df49fa28718360290b23b2fcdd3eab73b
seonjae <tjswo2292@naver.com>
2023-09-19 14:53:25 +0900

    feat: globalStyle, context Provider 추가

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { GlobalStyle } from './style/GlobalStyle.tsx'
+import ColorProvider from './context/colorProvider.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <ColorProvider>
+    <GlobalStyle />
+    <App />
+  </ColorProvider>
+)
