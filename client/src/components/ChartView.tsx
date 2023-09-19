6607b01c991a4a81be0d1d42e7da2583f3fb9ed1
seonjae <tjswo2292@naver.com>
2023-09-19 23:21:35 +0900

    bug: scales이 초기값으로 나오는 문제 해결

--- client/src/components/ChartView.tsx
+++ client/src/components/ChartView.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Chart } from 'react-chartjs-2'
-import { Chart as ChartJS, CategoryScale, LineController, BarController, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, Filler, BarElement, LogarithmicScale } from 'chart.js'
+import { Chart as ChartJS, CategoryScale, LineController, BarController, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, Filler, BarElement } from 'chart.js'
 import type { ChartData } from 'chart.js'
 import styled from 'styled-components'
 
@@ -8,7 +8,7 @@ import { option } from '../util/chartOption'
 import { TimeSeriesChartDataType } from '../types/TimeSeriesChartDataType'
 import { forwardRef } from 'react'
 
-ChartJS.register(CategoryScale, LineController, BarController, LinearScale, PointElement, LineElement, BarElement, LogarithmicScale, Title, Tooltip, Legend, Filler)
+ChartJS.register(CategoryScale, LineController, BarController, LinearScale, PointElement, LineElement, BarElement, Title, Tooltip, Legend, Filler)
 
 interface TimeSeriesChartProps {
   chartData: ChartData<'bar' | 'line', TimeSeriesChartDataType[]>
@@ -16,11 +16,7 @@ interface TimeSeriesChartProps {
 }
 
 const ChartView = forwardRef<any, TimeSeriesChartProps>(({ chartData, onClick }, ref) => {
-  return (
-    <Box>
-      <Chart onClick={onClick} type="bar" data={chartData} options={option} ref={ref} />
-    </Box>
-  )
+  return <Box>{chartData.labels?.length !== 0 && <Chart onClick={onClick} type="bar" data={chartData} options={option} ref={ref} />}</Box>
 })
 
 ChartView.displayName = 'ChartView'
