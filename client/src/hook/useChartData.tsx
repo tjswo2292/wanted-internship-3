faa1b361d99f11002ca97a6a09735e3c9bfb956d
seonjae <tjswo2292@naver.com>
2023-09-19 14:57:11 +0900

    feat: 차트를 직접 클릭시 하이라이트 로직 추가

--- client/src/hook/useChartData.tsx
+++ client/src/hook/useChartData.tsx
@@ -1,4 +1,6 @@
-import { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
+
+import { Chart as ChartJS } from 'chart.js'
 
 import { publicApi } from '../api/publicApi'
 import { getCharData } from '../util/getChartData'
@@ -6,16 +8,38 @@ import { ChartDataPropsType } from '../types/TimeSeriesChartDataType'
 import { ColorContext } from '../context/colorProvider'
 import { updateDatasets } from '../util/updateDatasets'
 import { removeDuplicatesArray } from '../util/removeDuplicatesArray'
+import type { InteractionItem } from 'chart.js'
+import { getElementsAtEvent } from 'react-chartjs-2'
 
 const useChartData = () => {
   const [chartData, setChartData] = useState<ChartDataPropsType>({ datasets: [] })
   const [labels, setLabels] = useState<string[]>([])
   const [regionForBtn, setRegionForBtn] = useState<string[]>([])
 
+  const chartRef = useRef<ChartJS>(null)
+
   const { colorArray, updateColorArray } = useContext(ColorContext)
 
   const handleRegionButton = (buttonTitle: string) => {
     updateColorArray(updateDatasets(buttonTitle, chartData))
+  }
+
+  const getClickedElement = (element: InteractionItem[]) => {
+    if (!element.length || chartData.datasets.length === 0) return
+    const { datasetIndex, index } = element[0]
+    const id = chartData.datasets[datasetIndex].data[index].y.id
+    return id
+  }
+
+  const highlightById = (filteredId: string | undefined) => {
+    updateColorArray(updateDatasets(filteredId, chartData))
+  }
+
+  const handleChartClick = (event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+    if (!chartRef.current) return
+    const clickedElementInCanvas = getElementsAtEvent(chartRef.current, event)
+    const id = getClickedElement(clickedElementInCanvas)
+    highlightById(id)
   }
 
   const regionItem = removeDuplicatesArray(regionForBtn)
@@ -53,12 +77,14 @@ const useChartData = () => {
   }, [])
 
   return {
+    chartRef,
     regionItem,
     chartDataProps: {
       labels,
       datasets: chartData.datasets,
     },
     handleRegionButton,
+    handleChartClick,
   }
 }
 
